49f1459c0edd13855cc7fb2cdd2cb4b75a3eb74c
Lakshya-Coder <lakshyagupta7089@gmail.com>
2021-04-23 12:35:20 +0530

    Some changes in ProgramToCheckStringsRotations.java

--- src/com/lakshya/String/ProgramToCheckStringsRotations.java
+++ src/com/lakshya/String/ProgramToCheckStringsRotations.java
@@ -4,21 +4,24 @@ package com.lakshya.String;
 
 public class ProgramToCheckStringsRotations {
 
-    public static void checkStringRotations(String str1, String str2) {
-        if (str1.length() != str2.length()) {
-            System.out.println("No");
-        } else {
-            String temp = str1 + str1;
-            if (temp.contains(str2)) {
-                System.out.println("Yes");
-            } else {
-                System.out.println("No");
-            }
-        }
+    public static boolean checkStringRotations(String str1, String str2) {
+//        if (str1.length() != str2.length()) {
+//            System.out.println("No");
+//        } else {
+//            String temp = str1 + str1;
+//            if (temp.contains(str2)) {
+//                System.out.println("Yes");
+//            } else {
+//                System.out.println("No");
+//            }
+//        }
+        return (str1.length() == str2.length()) && (str1.indexOf(str2) != -1);
     }
 
     public static void main(String[] args) {
-        checkStringRotations("ABACD", "CDABA");
+        boolean ans = checkStringRotations("ABACD", "ABACD");
+
+        System.out.println(ans);
     }
 
 }
