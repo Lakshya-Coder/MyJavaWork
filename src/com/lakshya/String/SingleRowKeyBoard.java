ff0474aded967487bfc25a2f453fba045439c2cc
Lakshya-Coder <lakshyagupta7089@gmail.com>
2021-04-26 04:48:12 +0530

    SingleRowKeyBoard.java

--- src/com/lakshya/String/SingleRowKeyBoard.java
+++ src/com/lakshya/String/SingleRowKeyBoard.java
@@ -3,10 +3,10 @@ package com.lakshya.String;
 public class SingleRowKeyBoard {
 	public static int calculateTime(String keyboard, String word) {
 		int time = 0;
-		int fromIndex = 0;
+		int fromIndex = 0; // In this variable we contain previous index c
 
 		for (char c : word.toCharArray()) {
-			int fingerMoves = Math.abs(fromIndex - keyboard.indexOf(c)); // Distance is always greate than or equal to 0
+			int fingerMoves = Math.abs(fromIndex - keyboard.indexOf(c)); // fingerMoves is always greate than or equal to 0
 			fromIndex = keyboard.indexOf(c);
 			time += fingerMoves;
 		}
