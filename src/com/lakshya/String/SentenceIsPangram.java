56f542fcb38eae1812793dbc443c2d84cff56f51
Lakshya-Coder <lakshyagupta7089@gmail.com>
2021-04-26 13:01:47 +0530

    SentenceIsPangram file added

---
+++ src/com/lakshya/String/SentenceIsPangram.java
@@ -0,0 +1,32 @@
+// package com.lakshya.String;
+
+public class SentenceIsPangram {
+	public static boolean checkIfPangram(String sentence) {
+//         Set<Character> set = new HashSet<>();
+        
+//         for (char c : sentence.toCharArray()) {
+//             set.add(c);
+//         }
+        
+//         return set.size() == 26;
+        sentence.replace(" ", "");
+        int[] arr = new int[26];
+        for (char c : sentence.toCharArray()) {
+            int index = c - 97;
+            arr[index] = 1;
+        }
+        
+        for (int val : arr) {
+            if (val == 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+	public static void main(String[] args) {
+		String sentence = "thequickbrownfoxjumpsoverthelazydog";
+		System.out.println(checkIfPangram(sentence));	
+	}
+}
