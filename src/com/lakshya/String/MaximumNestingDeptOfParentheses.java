344756d1aad86ed6920266b50d8e8dfd9fb21404
Lakshya-Coder <lakshyagupta7089@gmail.com>
2021-04-30 09:15:40 +0530

    -

--- src/com/lakshya/String/MaximumNestingDeptOfParentheses.java
+++ src/com/lakshya/String/MaximumNestingDeptOfParentheses.java
@@ -4,29 +4,27 @@ import java.util.Stack;
 
 public class MaximumNestingDeptOfParentheses {
     public static int maxDepth(String str) {
-        Stack<Character> stack = new Stack<>();
-        int count = 0;
+        int n = str.length();
 
-        // write code here
-        for (char c : str.toCharArray()) {
-            if (c == '(') {
-                stack.add(c);
-                System.out.println(":D");
-            } else if (c == ')') {
-                if (!stack.isEmpty() && stack.peek() == '(') {
-                    stack.pop();
-                    count++;
-                }
+        int count = 0; // Take care of all parentheses in string
+        int max_val = 0; // This is the result variable
+
+        for (int i = 0; i < n; i++) {
+            char curr = str.charAt(i);
+
+            if (curr == '(') {
+                count++;
+                max_val = Math.max(max_val, count);
+            } else if (curr == ')') {
+                count--;
             }
         }
 
-        System.out.println(stack);
-
-        return count;
+        return max_val;
     }
 
     public static void main(String[] args) {
-        String str = "(1)+((2))+(((3)))";
+        String str = "(1+(2*3)+((8)/4))+1";
         System.out.println(maxDepth(str));
     }
 }
