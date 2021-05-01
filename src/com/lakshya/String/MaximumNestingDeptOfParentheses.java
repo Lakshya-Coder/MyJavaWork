b588d33a29c32ceff2cd8c4c292f62660837b49e
Lakshya-Coder <lakshyagupta7089@gmail.com>
2021-05-01 10:21:29 +0530

    Some changes

--- src/com/lakshya/String/MaximumNestingDeptOfParentheses.java
+++ src/com/lakshya/String/MaximumNestingDeptOfParentheses.java
@@ -2,7 +2,21 @@ package com.lakshya.String;
 
 public class MaximumNestingDeptOfParentheses {
     public static int maxDepth(String str) {
+        // str == "" string is empty
+        if (str == null)
+            return -1;
+
         int n = str.length();
+
+        if (n == 0)
+            return 0;
+        else if (n == 1) {
+            boolean isOpenParentheses = str.charAt(0) == '(';
+            if (isOpenParentheses)
+                return 1;
+            else
+                return 0;
+        }
 
         int count = 0; // Take care of all parentheses in string
         int max_val = 0; // This is the result variable
