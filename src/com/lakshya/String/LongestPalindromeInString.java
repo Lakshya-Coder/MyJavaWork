03f42554e1181e7a22149f39cc9156714b6c5e9a
Lakshya-Coder <lakshyagupta7089@gmail.com>
2021-05-02 23:00:24 +0530

    Some changes

--- src/com/lakshya/String/LongestPalindromeInString.java
+++ src/com/lakshya/String/LongestPalindromeInString.java
@@ -1,62 +1,52 @@
 package com.lakshya.String;
 
 public class LongestPalindromeInString {
-    // This function prints the
-    // longest palindrome substring
-    // (LPS) of str[]. It also
-    // returns the length of the
-    // longest palindrome
-    static String longestPalin(String str)
-    {
-        // The result (length of LPS)
-        int maxLength = 1;
- 
+    public static String longestPalin(String s) {
+        // Base Case
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+
+        int len = s.length();
         int start = 0;
-        int len = str.length();
- 
+        int maxLength = 1;
         int low, high;
- 
-        // One by one consider every
-        // character as center
-        // point of even and length
-        // palindromes
-        for (int i = 1; i < len; ++i) {
-            // Find the longest even
-            // length palindrome with
-            // center points as i-1 and i.
+
+        for (int i = 1; i < len; i++) {
             low = i - 1;
             high = i;
-            while (low >= 0 && high < len
-                   && str.charAt(low)
-                          == str.charAt(high)) {
-                if (high - low + 1 > maxLength) {
+            while (low >= 0 && high < len &&
+                s.charAt(low) == s.charAt(high)) {
+                int currentLength = high - low + 1;
+                if (currentLength > maxLength) {
                     start = low;
-                    maxLength = high - low + 1;
+                    maxLength = currentLength;
                 }
-                --low;
-                ++high;
+
+                low--;
+                high++;
             }
- 
-            // Find the longest odd length
-            // palindrome with center point as i
+
             low = i - 1;
             high = i + 1;
-            while (low >= 0 && high < len
-                   && str.charAt(low)
-                          == str.charAt(high)) {
-                if (high - low + 1 > maxLength) {
+            while (low >= 0 && high < len &&
+                    s.charAt(low) == s.charAt(high)) {
+                int currentLength = high - low + 1;
+                if (currentLength > maxLength) {
                     start = low;
-                    maxLength = high - low + 1;
+                    maxLength = currentLength;
                 }
-                --low;
-                ++high;
+
+                low--;
+                high++;
             }
         }
- 
-        return str.substring(start,  start + maxLength);
+
+        return s.substring(start, start + maxLength);
     }
+
     public static void main(String[] args) {
-        System.out.println(longestPalin("aaaabbaa"));
+        System.out.println(longestPalin("aaaabbbaa"));
     }
 
 }
