c4c500d65c537d8e12365286ddf339d9c8975c63
Lakshya-Coder <lakshyagupta7089@gmail.com>
2021-05-02 21:21:25 +0530

    Some changes

--- src/com/lakshya/String/CountSay.java
+++ src/com/lakshya/String/CountSay.java
@@ -4,27 +4,39 @@ package com.lakshya.String;
 
 public class CountSay {
     public static String countAndSay(int n) {
+        /*
+
+        1
+        11
+        21
+        1211
+        111221
+        312211
+        13112221
+
+        */
         if (n == 1) return "1";
         if (n == 2) return "11";
 
-        String s = "11";
-        for (int i = 3; i <= n; i++) {
-            String t = "";
-            s = s + '&';
-            int c = 1;
-            for (int j = 1; j < s.length(); j++) {
-                if (s.charAt(j) != s.charAt(j-1)) {
-                    t = t + c;
-                    t = t + s.charAt(j-1);
-                    c = 1;
+        String string = "11";
+        System.out.println(string.length());
+        for (int i = 2; i < n; i++) {
+            String temp_str = "";
+            string = string + '&'; // This will work like a stopping point
+            int counter = 1; // Every value we encounter is at least comes 1 time
+            for (int j = 1; j < string.length(); j++) {
+                if (string.charAt(j) != string.charAt(j-1)) { // If we go in this condition so, we have encounter '&' or you say delimiter
+                    temp_str = temp_str + counter; // count process
+                    temp_str = temp_str + string.charAt(j-1 /* j-1 because jth character is this '&' just because we do j-1 */ ); // say process
+                    counter = 1; // set the default value of counter which is 1
                 } else {
-                    c++;
+                    counter++;
                 }
             }
-            s = t;
+            string = temp_str;
         }
 
-        return s;
+        return string;
     }
 
     public static void main(String[] args) {
