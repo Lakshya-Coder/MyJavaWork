e5a13b1f14cc83ce1a65d6fe1e3b265fb92b2c9f
Lakshya-Coder <lakshyagupta7089@gmail.com>
2021-05-04 11:00:06 +0530

    Some changes

---
+++ src/com/lakshya/String/LongestRepeatingSubsequence.java
@@ -0,0 +1,30 @@
+package com.lakshya.String;
+
+public class LongestRepeatingSubsequence {
+
+    public static int lcs(String a, String b) {
+        int[][] dp = new int[a.length() + 1][b.length() + 1];
+
+        for (int i = dp.length - 2; i >= 0; i--) {
+            for (int j = dp[0].length - 2; j >= 0; j--) {
+                char c1 = a.charAt(i);
+                char c2 = b.charAt(j);
+
+                if (c1 == c2 && i != j) {
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+
+    public static void main(String[] args) {
+        String str = "axxxy";
+
+        System.out.println(lcs(str, str));
+    }
+
+}
---
+++ src/com/lakshya/String/ToLowerCase.java
@@ -0,0 +1,12 @@
+package com.lakshya.String;
+
+public class ToLowerCase {
+    public static String toLowerCase(String str) {
+        return str.toLowerCase();
+    }
+
+    public static void main(String[] args) {
+        String str = "HELLO";
+        System.out.println(toLowerCase(str));
+    }
+}
